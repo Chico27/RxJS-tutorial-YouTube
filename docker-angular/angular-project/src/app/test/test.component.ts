f07e49145da49debac019d28384222a9cbafcf1c
Chico27 <masamasa.987654321@gmail.com>
2024-12-09 19:49:52 +0900

    エラーハンドリングの方法 #6

--- docker-angular/angular-project/src/app/test/test.component.ts
+++ docker-angular/angular-project/src/app/test/test.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { of, timer } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
-import { of } from 'rxjs';
-import { retry, catchError } from 'rxjs/operators';
+import { retry, catchError, delayWhen } from 'rxjs/operators';
 
 @Component({
   selector: 'app-test',
@@ -12,11 +12,17 @@ import { retry, catchError } from 'rxjs/operators';
 })
 export class TestComponent implements OnInit {
   ngOnInit() {
-    const apiRequest$ = ajax('https://pokeapi.co/api/v2/pokemon/1');
+    const apiRequest$ = ajax('https://pokeapi.co/api/v2/pokem2on/1');
 
     apiRequest$
       .pipe(
-        retry(2), // 最大2回まで再試行
+        retry({
+          count: 3, // 最大3回まで再試行
+          delay: (error, retryCount) => {
+            console.log(`Retrying... Attempt: ${retryCount}`);
+            return timer(2000); // 2秒待って再試行
+          }
+        }),
         catchError(err => {
           console.error('API Error:', err);
           return of({ data: [] }); // 空のデータを返す
